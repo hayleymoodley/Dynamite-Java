da1d42e3ef586f95e8019a2f66b0d9809df3d54e
hayleymoodley <moodley.hayley@gmail.com>
2020-10-02 11:32:10 +0100

    Got the game working - added conditions for playing certain moves

--- src/training/dynamite/MyBot.java
+++ src/training/dynamite/MyBot.java
@@ -3,19 +3,64 @@ package training.dynamite;
 import com.softwire.dynamite.bot.Bot;
 import com.softwire.dynamite.game.*;
 
+
 public class MyBot implements Bot {
 
     public MyBot() {
-        // Are you debugging?
-        // Put a breakpoint on the line below to see when we start a new match
         System.out.println("Started new match");
     }
 
     @Override
     public Move makeMove(Gamestate gamestate) {
-        // Are you debugging?
-        // Put a breakpoint in this method to see when we make a move
-        return Move.R;
+        if (gamestate.getRounds().size() == 0) {
+            return Move.D;
+        } else {
+            Round lastRound = gamestate.getRounds().get(gamestate.getRounds().size() - 1);
+            return getMoveThatBeats(lastRound.getP2());
+        }
     }
 
+    private Move getMoveThatBeats(Move theirLastMove) {
+        switch (theirLastMove) {
+            case R:
+                return Move.P;
+            case P:
+                return Move.S;
+            case S:
+                return Move.R;
+            case D:
+                return Move.W;
+            case W:
+                return getRandomMove();
+            default:
+                throw new RuntimeException("INVALID");
+        }
+    }
+
+    public Move getRandomMove() {
+        int randomNumberBetween0And3 = (int) Math.floor(Math.random() * 3.0D);
+        Move[] possibleMoves = new Move[]{Move.R, Move.P, Move.S};
+        return possibleMoves[randomNumberBetween0And3];
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
